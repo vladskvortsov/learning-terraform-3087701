1a8a4126ac190493dd1a96a403a564ae2eaf6c2d
vladskvortsov <vladskvortsov96@gmail.com>
2024-08-19 11:29:05 +0300

    variables enhanced

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner] 
 }
 
 data "aws_vpc" "default" {
@@ -21,35 +21,38 @@ data "aws_vpc" "default" {
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
   name = "dev"
-  cidr = "10.0.0.0/16"
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  name = "${var.environment.name}-blog"
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  min_size            = var.asg_min_size
+  max_size            = var.asg_max_size
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "blog-alb"
+  name = "${var.environment.name}-blog-alb"
 
   load_balancer_type = "application"
 
@@ -59,7 +62,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.environment.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -75,7 +78,7 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -83,7 +86,7 @@ module "blog_alb" {
 module "blog_sg" {
   source              = "terraform-aws-modules/security-group/aws"
   version             = "5.1.2"
-  name                = "blog_new" 
+  name                = "${var.environment.name}-blog" 
   
   vpc_id              = module.blog_vpc.vpc_id
   
--- variables.tf
+++ variables.tf
@@ -2,3 +2,44 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+
+variable "ami_filter" {
+
+  description = "Name filter and owner"
+  type    = object({
+    name  = string
+    owner = string
+  })
+
+  default = {
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
+  }
+
+
+}
+
+variable "environment" {
+  description = "Development environment"
+  type = object({
+    name = string
+    network_prefix = string
+  })
+  
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
+  }
+}
+ 
+variable "asg_min_size" {
+  description = "Min number of instances in ASG"
+  default     = 1
+}
+
+variable "asg_max_size" {
+  description = "Max number of instances in ASG"
+  default     = 2
+}
+
