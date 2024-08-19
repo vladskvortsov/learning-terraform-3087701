22f928b717bec863c15dfdc692f4ba1e0ed20fb0
vladskvortsov <vladskvortsov96@gmail.com>
2024-08-19 13:06:38 +0300

    define qa environment

---
+++ qa/main.tf
@@ -0,0 +1,12 @@
+module  "qa" {
+    source = "../modules/blog/"
+
+environment = {
+  name           = "qa"
+  network_prefix = "10.1"
+  }
+
+  asg_min_size = 0
+  asg_max_size = 0
+}  asg_min_size = 1
+  asg_max_size = 1
